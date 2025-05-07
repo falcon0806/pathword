ce2183840aad582f6ecf3c1264f335a01ef47a98
Akash <akasram@gmail.com>
2025-05-08 00:28:32 +0530

    Initial commit - working Pathword game

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@
 const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/pathword' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/pathword/' : '',
   images: { unoptimized: true },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
